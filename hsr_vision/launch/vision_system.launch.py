a6af8e2784e0f67c6390525c4e969d6fe3ad3989
AndrewSnowdy <drewsnowdy@hotmail.com>
2026-03-08 13:35:17 -0400

    fixed up vision launch for ssh to jetson

--- hsr_vision/launch/vision_system.launch.py
+++ hsr_vision/launch/vision_system.launch.py
@@ -6,13 +6,14 @@ def generate_launch_description():
     # 1. Jetson Remote Inference
     # We chain the commands: source the workspace, THEN run the node.
     # Replace '~/hsr_ros2_ws' with the actual path on your Jetson if it differs.
+    # need to run ssh-copy-id neuroam@192.168.200.201 first to set up passwordless ssh
     start_jetson_inference = ExecuteProcess(
         cmd=[
-            'ssh', '-t', 'administrator@192.168.200.201', 
-            'ros2 run hsr_vision inference_node'
+            'ssh', '-tt', 'neuroam@192.168.200.201', 
+            'export ROS_DOMAIN_ID=48 && source /opt/ros/humble/setup.bash && source ~/hsr_ros2_ws/install/setup.bash && ros2 run hsr_vision inference_node'
         ],
-        output='screen',
-        shell=True
+        output='screen'
+        # shell=True REMOVED - ROS 2 will handle the execution directly
     )
 
     # 2. HSR Local Processing
